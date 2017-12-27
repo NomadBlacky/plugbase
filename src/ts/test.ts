407b0910300212f2b7c04abf27a31aa7ecf06781
aiya000 <aiya000.develop@gmail.com>
2017-12-27 21:49:55 +0900

    Change indentation from 4 spaces to 2 spaces 💣

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -1,4 +1,4 @@
 const greeting = document.getElementById('greeting');
 if (greeting != null) {
-    greeting.innerText = 'Haskell';
+  greeting.innerText = 'Haskell';
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const Path = require('path')
 
 module.exports = [
-    {
-        context: Path.join(__dirname, 'src/ts'),
-        entry: './test.ts',
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                }
-            ]
-        },
-        output: {
-            filename: 'bundle.js',
-            path: Path.resolve(__dirname, 'dist/js'),
-        },
+  {
+    context: Path.join(__dirname, 'src/ts'),
+    entry: './test.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        }
+      ]
     },
+    output: {
+      filename: 'bundle.js',
+      path: Path.resolve(__dirname, 'dist/js'),
+    },
+  },
 ];
