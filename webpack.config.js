7390c201f0c82ae20f04e31b6540136a0bc7167a
aiya000 <aiya000.develop@gmail.com>
2017-12-27 19:45:51 +0900

    Move .ts from src to src/ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const Path = require('path')
 
-module.exports = {
-    entry: './src/test.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+module.exports = [
+    {
+        context: Path.join(__dirname, 'src/ts'),
+        entry: './test.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                }
+            ]
+        },
+        output: {
+            filename: 'bundle.js',
+            path: Path.resolve(__dirname, 'dist'),
+        },
     },
-    output: {
-        filename: 'bundle.js',
-        path: Path.resolve(__dirname, 'dist')
-    }
-};
+];
